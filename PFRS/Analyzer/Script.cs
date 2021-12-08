36b31bbd594d9c2c3e0f8215989aabfdbefc04ba
Lukáš Salak <lukas.salak1@gmail.com>
2021-12-09 00:57:35 +0100

    Started implementing basic shape of Script class
    Script class represents how the script editor in PFRS project works, how is stored and edited and saved

---
+++ PFRS/Analyzer/Script.cs
@@ -0,0 +1,21 @@
+﻿namespace Analyzer;
+public class Script
+{
+    public bool Saved { get => this.Contents == this.WorkingContents; }
+    public string Contents;
+    public int ID { get; set; }
+    public object Name { get; set; }
+    public string WorkingContents { get; set; }
+
+    public string Path;
+    public Script(int ID, string Path, string fileContent)
+    {
+        this.ID = ID;
+        this.Path = Path;
+        this.Contents = fileContent;
+        this.Name = Path.Split("\\")[^1];
+        this.WorkingContents = this.Contents;
+    }
+
+}
+
