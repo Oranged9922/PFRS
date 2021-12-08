0ecc9188531d4d6294e07148ebe8ae38b99c6456
Lukáš Salak <lukas.salak1@gmail.com>
2021-12-09 00:57:50 +0100

    TODO: not working, imports are wrong for some reason

--- PFRS/Analyzer/ScriptAnalyzer.cs
+++ PFRS/Analyzer/ScriptAnalyzer.cs
@@ -1,6 +1,52 @@
-﻿
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Reflection;
+
 namespace PFRS.Analyzer;
 public class ScriptAnalyzer
 {
+    private readonly List<Assembly> assemblies;
+    private readonly List<string> imports;
+    public ScriptAnalyzer()
+    {
+        this.assemblies = new();
+        // add all assemblies
+        assemblies.Add(Assembly.GetExecutingAssembly());
+        var assemblyNames = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
+        foreach (var assemblyName in assemblyNames)
+            assemblies.Add(Assembly.Load(assemblyName));
 
+        this.imports = new()
+        {
+            "System",
+            "PFRS",
+            "Common.HardwareRepresentation"
+        };
+
+
+    }
+
+    public Task CompileScript(string sourceCode)
+    {
+        return CSharpScript.RunAsync(
+            sourceCode,
+            globals: null,
+            options: ScriptOptions.Default.WithReferences(assemblies).AddImports(imports));
+    }
+}
+
+/// <summary>
+/// Global variables for formula scripts.
+/// </summary>
+public class Globals
+{
+    /// <summary>
+    /// Optional text parameter (usually from form's 'Params:' field).
+    /// </summary>
+    public string param;
+
+    /// <summary>
+    /// Parameter map for passing values in/out of the script.
+    /// </summary>
+    public Dictionary<string,object> context;
 }
