9dc90843053a4afc631b65554874efbafecd1015
Lukáš Salak <lukas.salak1@gmail.com>
2021-12-09 21:46:07 +0100

    Added utility function Clamp and class Vector2D

--- PFRS/Utils/Utils.cs
+++ PFRS/Utils/Utils.cs
@@ -1,7 +1,25 @@
-﻿namespace Utils
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
 {
     public class Utils
     {
 
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            if (value > max)
+            {
+                return max;
+            }
+            return value;
+        }
     }
 }
---
+++ PFRS/Utils/Vector.cs
@@ -0,0 +1,29 @@
+﻿namespace Utils.Vector;
+public struct Vector2
+{
+    public static Vector2 Zero { get; }
+    public double X;
+    public double Y;
+
+    public static double DotProduct(Vector2 vector, Vector2 other) 
+        => vector.X * other.X + vector.Y * other.Y;
+    public double LengthSquared { get => DotProduct(this,this); }
+    public double Length { get => Math.Sqrt(LengthSquared); }
+
+    public Vector2(double x, double y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    static Vector2()
+    {
+        Zero = new Vector2(0,0);
+    }
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
+
