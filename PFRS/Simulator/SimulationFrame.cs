eb9c3dcc8641d6c96027fb4160d343078058a5e9
Lukáš Salak <lukas.salak1@gmail.com>
2021-12-12 16:28:40 +0100

    Added field sensorsReadings array and forloop into constructor that fills the array

--- PFRS/Simulator/SimulationFrame.cs
+++ PFRS/Simulator/SimulationFrame.cs
@@ -5,10 +5,17 @@ namespace Simulator
     public struct SimulationFrame
     {
         public RobotCoordinates RobotCoordinates;
+        public float[] sensorsReadings;
         public SimulationFrame(IRobot robot, int frameNumber)
         {
+            sensorsReadings= new float[robot.RobotInfo.Sensors.Count];
             FrameNumber = frameNumber;
             this.RobotCoordinates = robot.RobotCoordinates;
+
+            for (int i = 0; i < robot.RobotInfo.Sensors.Count; i++)
+            {
+                sensorsReadings[i] = robot.RobotInfo.Sensors[i].GetReading();
+            }
         }
 
         public int FrameNumber { get; internal set; }
