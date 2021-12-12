3b44ad72d13540141953c9e8c6c22e501149b620
Lukáš Salak <lukas.salak1@gmail.com>
2021-12-12 16:24:12 +0100

    Implemented GetReading method

--- PFRS/Common/HardwareRepresentation/OpticalSensor.cs
+++ PFRS/Common/HardwareRepresentation/OpticalSensor.cs
@@ -8,7 +8,12 @@ namespace Common.HardwareRepresentation
 
         public float GetReading()
         {
-            throw new NotImplementedException();
+            Vector2 absolutePositon 
+                = (MountedOn.RobotCoordinates.Position + 
+                    (relPosFromRobotCenter * new Vector2(MountedOn.SizeX, MountedOn.SizeY)));
+            absolutePositon = absolutePositon + new Vector2(MountedOn.SizeX/2, MountedOn.SizeY/2);
+            var track = MountedOn.Track;
+            return track[(int)absolutePositon.X, (int)absolutePositon.Y] == -1 ? 0 :1;
         }
     }
 }
