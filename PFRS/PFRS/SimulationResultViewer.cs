d223cb7f1bf622d67a875706ba92e70113e7073e
Lukáš Salak <lukas.salak1@gmail.com>
2021-12-21 02:48:12 +0100

    Changed the way how final image is rendered, also added minor details (toggling between render only sensors/render whole robot) (TODO, doesn't work properly)

--- PFRS/PFRS/SimulationResultViewer.cs
+++ PFRS/PFRS/SimulationResultViewer.cs
@@ -15,46 +15,105 @@ namespace PFRS
     {
 
         Bitmap Robot;
+        Bitmap Track;
+        bool sensorsOnly = false;
         List<Simulator.SimulationFrame> simulationFrames;
-        float prevAngle;
-        public SimulationResultViewer(Bitmap Track, Bitmap Robot, List<Simulator.SimulationFrame> simFrames)
+        //float prevAngle;
+        public SimulationResultViewer(Bitmap Track, Bitmap Robot, List<Simulator.SimulationFrame> simFrames, (string SelectedScript, string SelectedTrack, string SelectedRobot) WindowTitle)
         {
             this.simulationFrames = simFrames;
-            prevAngle = (float)simFrames[0].RobotCoordinates.RotationAngle;
+            //prevAngle = (float)simFrames[0].RobotCoordinates.RotationAngle;
 
             InitializeComponent();
             this.LabelEnd.Text = simFrames.Count.ToString();
             Slider.Maximum = simFrames.Count-1;
             Slider.Minimum = 0;
-            PictureBoxRobot.BackColor = Color.Transparent;
-            PictureBoxRobot.Parent = PictureBoxTrack;
+            //PictureBoxRobot.BackColor = Color.Transparent;
+            //PictureBoxRobot.Parent = PictureBoxTrack;
+            //PictureBoxSensorsCoords.BackColor = Color.Transparent;
+            //PictureBoxSensorsCoords.Parent = PictureBoxTrack;
             this.Robot = Robot;
-            PictureBoxRobot.Image = RotateImage(Robot, (float)simulationFrames[0].RobotCoordinates.RotationAngle); ;
-            PictureBoxTrack.Image = Track;
+            this.Track = Track;
+            //PictureBoxRobot.Image = RotateImage(Robot, (float)simulationFrames[0].RobotCoordinates.RotationAngle); ;
+            //PictureBoxTrack.Image = Track;
 
-            var pos = simulationFrames[0].RobotCoordinates.Position;
-            PictureBoxRobot.Location = new Point((int)pos.X-Robot.Width, (int)pos.Y-Robot.Height);
+            SetText(0);
+            PictureBoxSimulationFrame.Image = GenerateImage(0);
+            motorsInfo.Text = $"{String.Concat(simFrames[0].MotorsSpeed.Select(x => x.ToString() + ", ")).TrimEnd(' ').TrimEnd(',')}";
+            this.Text = $"Simulation of script \"{WindowTitle.SelectedScript}\", using {WindowTitle.SelectedRobot} on {WindowTitle.SelectedTrack}";
+            //var pos = simulationFrames[0].RobotCoordinates.Position;
+            //PictureBoxRobot.Location = new Point((int)pos.X, (int)pos.Y);
 
         }
 
         private void Slider_Scroll(object sender, EventArgs e)
         {
-            this.LabelFrameNumber.Text = 
-                simulationFrames[Slider.Value].FrameNumber.ToString();
-            this.LabelDirection.Text = 
-                ((int)simulationFrames[Slider.Value].RobotCoordinates.RotationAngle).ToString();
-            this.LabelPosition.Text = 
-                simulationFrames[Slider.Value].RobotCoordinates.Position.AsInt().ToString();
-            this.LabelSensors.Text = String.Concat(simulationFrames[Slider.Value].sensorsReadings);
-            var pos = simulationFrames[Slider.Value].RobotCoordinates.Position;
+            SetText(Slider.Value);
+            PictureBoxSimulationFrame.Image = GenerateImage(Slider.Value);
+            //var pos = simulationFrames[Slider.Value].RobotCoordinates.Position;
+            //PictureBoxRobot.Image = RotateImage(Robot, (float)simulationFrames[Slider.Value].RobotCoordinates.RotationAngle);
+            ////prevAngle = (float)simulationFrames[Slider.Value].RobotCoordinates.RotationAngle;
+            //PictureBoxRobot.Location = new Point
+            //    (
+            //        (int)pos.X, 
+            //        (int)pos.Y
+            //    );
 
-            PictureBoxRobot.Image = RotateImage(Robot, (float)simulationFrames[Slider.Value].RobotCoordinates.RotationAngle);
-            prevAngle = (float)simulationFrames[Slider.Value].RobotCoordinates.RotationAngle;
-            PictureBoxRobot.Location = new Point
-                (
-                    (int)pos.X-Robot.Width, 
-                    (int)pos.Y-Robot.Height
-                );
+            //Bitmap bmp = new Bitmap(PictureBoxSensorsCoords.Width, PictureBoxSensorsCoords.Height);
+            //bmp.MakeTransparent();
+            //Graphics g = Graphics.FromImage(bmp);
+            //Pen p = new Pen(Color.Red);
+            //foreach (var SensorPos in simulationFrames[Slider.Value].SensorsCoordinates)
+            //{
+            //    g.DrawRectangle(p, (int)SensorPos.X, (int)SensorPos.Y, 2,2);
+
+            //}
+            //g.Dispose();
+            //bmp.MakeTransparent();
+            //PictureBoxSensorsCoords.Image = bmp;
+
+        }
+
+        private void SetText(int value)
+        {
+            LabelFrameNumber.Text =
+                simulationFrames[Slider.Value].FrameNumber.ToString();
+            LabelDirection.Text =
+                ((int)simulationFrames[Slider.Value].RobotCoordinates.RotationAngle).ToString();
+            LabelPosition.Text =
+                simulationFrames[Slider.Value].RobotCoordinates.Position.AsInt().ToString();
+            LabelSensors.Text = String.Concat(simulationFrames[Slider.Value].SensorsReadings);
+            Sensors.Text =
+                String.Concat(simulationFrames[Slider.Value].SensorsCoordinates.Select(x => $"({(int)x.X},{(int)x.Y})\n"));
+            motorsInfo.Text = $"{String.Concat(simulationFrames[Slider.Value].MotorsSpeed.Select(x => x.ToString() + ", ")).TrimEnd(' ').TrimEnd(',')}";
+        
+        }
+
+        private Image GenerateImage(int frame)
+        {
+            Bitmap bmp = new(Track);
+            Graphics graphics = Graphics.FromImage(bmp);
+            if (sensorsOnly)
+            {
+                 Pen p = new Pen(Color.Red);
+                foreach (var SensorPos in simulationFrames[Slider.Value].SensorsCoordinates)
+                {
+                    graphics.DrawRectangle(p, (int)SensorPos.X, (int)SensorPos.Y, 2, 2);
+
+                }
+            }
+            else
+            {
+                Image rotatedRobot = RotateImage(Robot, (float)simulationFrames[frame].RobotCoordinates.RotationAngle);
+                graphics.DrawImage(rotatedRobot,
+                    new Rectangle((int)simulationFrames[frame].RobotCoordinates.Position.X - Robot.Width / 2,
+                                  (int)simulationFrames[frame].RobotCoordinates.Position.Y - Robot.Height / 2,
+                                  rotatedRobot.Width, rotatedRobot.Height));
+
+            }
+            graphics.Dispose();
+
+            return bmp;
         }
 
 
@@ -67,7 +126,7 @@ namespace PFRS
             //turn the Bitmap into a Graphics object
             Graphics gfx = Graphics.FromImage(bmp);
             Matrix transformM = new Matrix();
-            transformM.RotateAt(-rotationAngle, new((float)bmp.Width / 2, (float)bmp.Height / 2));
+            transformM.RotateAt(rotationAngle, new((float)bmp.Width / 2, (float)bmp.Height / 2));
 
             //now we set the rotation point to the center of our image
             //gfx.TranslateTransform((float)bmp.Width / 2, (float)bmp.Height / 2);
@@ -91,6 +150,12 @@ namespace PFRS
 
             //return the image
             return bmp;
+        }
+
+        private void SensorsOnly_Click(object sender, EventArgs e)
+        {
+            sensorsOnly = sensorsOnly ? false : true; 
+            PictureBoxSimulationFrame.Image = GenerateImage(Slider.Value);
         }
     }
 }
